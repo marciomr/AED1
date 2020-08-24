4447cbbf7ccc2b771160191e2f9c7b0acd718555
Marcio M. Ribeiro <marcio.mr@gmail.com>
2020-08-24 17:32:46 -0300

    Nos com duas ligações

---
+++ Capitulo 3.3/DNode.java
@@ -0,0 +1,18 @@
+public DNode{
+  protected String element;
+  protected DNode prev, next;
+
+  public DNode(String e, DNode p, DNode n){
+    element = e;
+    prev = p;
+    next = n;
+  }
+
+  public String getElement(){ return element; }
+  public DNode getPrev(){ return prev; }
+  public DNode getNext(){ return next; }
+
+  public void setElement(String newElement){ element = newElement; }
+  public void setPrev(DNode newPrev){ prev = newPrev; }
+  public void setPrev(DNode newNext){ next = newNext; }  
+}
