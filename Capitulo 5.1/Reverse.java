7d299869c530c0a8c275442979f53bbac98a22a9
Marcio M. Ribeiro <marcio.mr@gmail.com>
2020-08-25 02:12:59 -0300

    Exemplo do uso de pilha para reverter um arranjo

---
+++ Capitulo 5.1/Reverse.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Reverse {
+  public static <E> void reverse(E[] a){
+    Stack<E> S = new ArrayStack<E>(a.length);
+    for(E element : a) S.push(element);
+    for(int i = 0; i < a.length; i++) a[i] = S.pop();
+  }
+
+  public static void main(String[] args) {
+    Integer[] a = {4, 8, 15, 16, 23, 42};
+    System.out.println(Arrays.toString(a));
+    reverse(a);
+    System.out.println(Arrays.toString(a));
+  }
+}
