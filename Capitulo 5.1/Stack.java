b73c418c6b408077e76eae0722c09ce9f8a1dfb1
Marcio M. Ribeiro <marcio.mr@gmail.com>
2020-09-16 15:30:47 -0300

    Comentário na interface de uma pilha genérica

--- Capitulo 5.1/Stack.java
+++ Capitulo 5.1/Stack.java
@@ -1,7 +1,16 @@
 interface Stack<E> {
+  // devolve o tamanho da pilha
   public int size();
+
+  // verifica se a pilha está vazia
   public boolean isEmpty();
+
+  // devolve o elemento no topo da pilha
   public E top() throws EmptyStackException;
+
+  // insere um elemento no topo da pilha
   public void push(E element);
+
+  // remove e devolve o elemento no topo da pilha
   public E pop() throws EmptyStackException;
 }
