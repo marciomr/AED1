a62203fd4b3cb3c6001faf357d96b40e441c910d
Marcio M. Ribeiro <marcio.mr@gmail.com>
2020-09-30 15:31:33 -0300

    Documentação da interface da fila.

--- Capitulo 5.2/Queue.java
+++ Capitulo 5.2/Queue.java
@@ -1,7 +1,16 @@
 public interface Queue<E> {
+  // devolve o tamanho da fila
   public int size();
+
+  // verifica se a fila está vazia
   public boolean isEmpty();
+
+  // devolve o primeiro da fila
   public E front() throws EmptyQueueException;
+
+  // insere um elemento no fim da fila
   public void enqueue(E element);
+
+  // remove e devolve o primeiro da fila
   public E dequeue() throws EmptyQueueException;
 }
